59f60bce33554849a771923a64a60a25c7839fce
AJanasik <ajanasik1@gmail.com>
2023-05-04 13:56:05 +0200

    ok

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+let intervalId = null;
+
+function startColorSwitching() {
+  startButton.disabled = true;
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopColorSwitching() {
+  startButton.disabled = false;
+  clearInterval(intervalId);
+  intervalId = null;
+}
+
+startButton.addEventListener('click', startColorSwitching);
+stopButton.addEventListener('click', stopColorSwitching);
